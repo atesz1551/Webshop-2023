7a0a33c4ddc3ee9bd86068c5c76991b86ecc10f8
Kasza Attila <kaszaattila@stud.uni-obuda.hu>
2023-12-08 20:35:58 +0100

    cartitem

--- Webshop/Models/CartItem.cs
+++ Webshop/Models/CartItem.cs
@@ -2,29 +2,26 @@
 {
     public class CartItem
     {
-        public class CartItem
+        public long ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total
         {
-            public long ProductId { get; set; }
-            public string ProductName { get; set; }
-            public int Quantity { get; set; }
-            public decimal Price { get; set; }
-            public decimal Total
-            {
-                get { return Quantity * Price; }
-            }
-            public string Image { get; set; }
+            get { return Quantity * Price; }
+        }
+        public string Image { get; set; }
 
-            public CartItem()
-            { }
+        public CartItem()
+        { }
 
-            public CartItem(Product product)
-            {
-                ProductId = product.Id;
-                ProductName = product.Name;
-                Price = product.Price;
-                Quantity = 1;
-                Image = product.Image;
-            }
+        public CartItem(Product product)
+        {
+            ProductId = product.Id;
+            ProductName = product.Name;
+            Price = product.Price;
+            Quantity = 1;
+            Image = product.Image;
         }
     }
 }
