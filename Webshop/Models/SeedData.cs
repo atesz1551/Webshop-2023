9333df6e74a4ad7b3a75412395f942b4f655a226
Kasza Attila <kaszaattila@stud.uni-obuda.hu>
2023-12-08 18:25:56 +0100

    SeedData

---
+++ Webshop/Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Webshop.DAL;
+
+namespace Webshop.Models
+{
+    public class SeedData
+    {
+        public static void SeedDatabase(DataContext context)
+        {
+            //context.Database.Migrate();
+
+            if (!context.Products.Any())
+            {
+                Category MaleShirts = new Category { Name = "Férfi pólók", Slug = "ferfi_polo" };
+                Category FemaleShirts = new Category { Name = "Női pólók", Slug = "noi_polo" };
+
+                context.Products.AddRange(
+                        new Product
+                        {
+                            Name = "3-STRIPES TEE - Nyomott mintás póló",
+                            Slug = "ferfi_adidas",
+                            Description = "Külső szövet anyaga: \r\n    100% pamut\r\nSzövet: \r\n    Dzsörzé\r\nKezelési utasítások: \r\n    Gépi mosás 30 °C-on, gépi mosás kíméletes programon",
+                            Price = 11690M,
+                            Category = MaleShirts,
+                            Image = "adidas_3_stripes_green.jpg"
+                        }
+                        
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
+}
