d13f6bc04b9c6afa356ad3f53cf59ca77106b4ac
Kasza Attila <kaszaattila@stud.uni-obuda.hu>
2023-12-08 18:16:29 +0100

    Add project files.

---
+++ Webshop/DAL/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webshop.Models;
+
+namespace Webshop.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
